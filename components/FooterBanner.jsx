ede09be18edf392716b01438c94ed8afdd6bef51
w.benarto <w.benarto@gmail.com>
2022-08-19 18:42:18 -0700

    landing page done

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
 const FooterBanner = () => {
     return (
-        <div className='footer-banner-container'>
-            <div className='left'>
-                <h3>All Smiles</h3>
-            </div>
-            <div className='right'>
-
+        <div className='h-[400px] w-full py-10 px-10'>
+            {/* <div className='left'>
+                <h3>Maps</h3>
+            </div> */}
+            <div className='text-center'>
+                <h3 className='pb-10 text-4xl font-display'>Contact Us</h3>
+                <div>Email Us</div>
+                <div>Instagram</div>
+                <div>Instagram</div>
             </div>
         </div>
     )
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -5,20 +5,21 @@ import { urlFor } from '../lib/client'
 
 const HeroBanner = ({ heroBanner }) => {
     return (
-        <div className='hero-banner-container'>
-            <div>
-                <p className='beats-solo'>{heroBanner.smallText}</p>
-                <h3>Mid Text</h3>
-                <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image'></img>
+        <div className='hero-banner-container '>
+            <img className='contrast-50 object-cover h-[500px] w-full ' src={'/images/flower.jpg'}></img>
+            <div className='top-0  h-1/2 w-full absolute text-white text-center'>
 
-                <div>
+                <h5 className='my-24 font-raleway text-4xl'>CELESTE CAKESHOP</h5>
+
+                <h5 className='mb-10 font-body font-light text-5xl'>ORLANDO'S BEST DESIGNED <br /> BAKED GOODS</h5>
+                <p className='font-display'>Beautifully crafted sweet treats for your sweetest moments.</p>
+
+
+                <div className='relative'>
                     <Link href={`/product/${heroBanner.product}`}>
                         <button type='button'>{heroBanner.buttonText}</button>
                     </Link>
-                    <div className='desc'>
-                        <h5>Description</h5>
-                        <p>description</p>
-                    </div>
+
 
                 </div>
             </div>
