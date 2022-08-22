5f9b2c4e6190ac0f29096f94bc7f827422b461b3
w.benarto <w.benarto@gmail.com>
2022-08-22 16:39:45 -0700

    layout;

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -9,13 +9,13 @@ const HeroBanner = ({ heroBanner }) => {
             <img className='contrast-50 object-cover h-[500px] w-full ' src='/images/flower.jpg'></img>
             <div className='top-0  h-1/2 w-full absolute text-white text-center'>
 
-                <h5 className='my-24 font-raleway text-4xl'>CELESTE CAKESHOP</h5>
+                <h5 className='my-24 font-raleway text-2xl md:text-4xl'>CELESTE CAKESHOP</h5>
 
-                <h5 className='mb-10 font-body font-light text-5xl'>ORLANDO'S BEST DESIGNED <br /> BAKED GOODS</h5>
+                <h5 className='mb-10 font-body font-light text-2xl md:text-5xl'>ORLANDO'S BEST DESIGNED <br /> BAKED GOODS</h5>
                 <p className='font-display'>Beautifully crafted sweet treats for your sweetest moments.</p>
 
 
-                <div className='bg-red-300 w-[150px] h-14 mx-auto border-2 rounded-md mt-4 text-center flex justify-center items-center'>
+                <div className='bg-red-300 w-24 h-10 md:w-[150px] md:h-14 mx-auto border-2 rounded-md mt-4 text-center flex justify-center items-center'>
                     <Link href={`/product/${heroBanner.product}`}>
                         <button type='button'>{heroBanner.buttonText}</button>
                     </Link>
--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,15 @@ const Home = ({bannerData, products}) => {
      
       <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
       <div className='h-8 w-full text-white grid content-center text-center bg-gradient-to-r from-[#f7abdb] to-purple-500 '>
-        <h1 className='self-center font-body '>
+        <h1 className='self-center text-xs md:text-md font-body '>
           Baked with the finest natural ingredients. 
         </h1>
       </div>
    
-        <div className='flex flex-row my-20 h-96 px-32 sm:px-10'>
-          <div className='w-1/2 my-auto'>
-            <h2 className='text-red-300 text-6xl'>Custom Cake Order</h2>
-            <p className='py-10 '>We take Custom Design Cake pre-orders for your special occassion. Send us your design! </p>
+        <div className='flex flex-row my-2 py-10 md:my-20 h-96 px-4 md:px-32 '>
+          <div className='w-1/2 my-auto flex-column content-start'>
+            <h2 className='text-red-300 text-xl font-bold md:text-6xl'>Custom Cake Order</h2>
+            <p className='py-4 md:py-10 md:text-lg text-xs '>We take Custom Design Cake pre-orders for your special occassion. Send us your design! </p>
             <h1 className='font-bold'>Made especially for you</h1>
           </div>
           <div className='w-1/2 flex justify-end '>
@@ -28,8 +28,8 @@ const Home = ({bannerData, products}) => {
         </div>
       
       
-      <div className='my-20 text-center '>
-        <h2 className=' text-red-300 text-4xl'>Other Baked Goods</h2>
+      <div className='my-4 md:my-20 text-center '>
+        <h2 className=' text-red-300 text-xl font-bold md:text-4xl'>Other Baked Goods</h2>
         <div className='flex flex-wrap justify-center gap-[15px] mt-10 w-full'>
           {products.map((e,i) => (
             <Product product={e} key={e.id} />
