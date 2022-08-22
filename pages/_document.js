d2d82660a6bec55bc2f0b739d40745ee0af30a52
w.benarto <w.benarto@gmail.com>
2022-08-21 23:07:09 -0700

    v1 test

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
   render() {
     return (
       <Html>
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=BIZ+UDMincho&family=Poiret+One&family=Raleway:wght@200;400&display=swap" rel="stylesheet" />
         {/* <script
             src={`https://maps.googleapis.com/maps/api/js?key=${settings?.googleMaps?.apiKey}&callback=initMap&libraries=&v=weekly`}
