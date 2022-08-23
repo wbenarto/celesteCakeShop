86cff9c7a18068a65247969c7d1215aabafa7d0d
w.benarto <w.benarto@gmail.com>
2022-08-22 17:03:57 -0700

    mobile

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -5,13 +5,13 @@ import { AiFillInstagram, AiFillMail } from 'react-icons/ai'
 
 const FooterBanner = () => {
     return (
-        <div className='md:h-[400px] w-full py-10 sm:py-2 sm:px-6 px-10 md:flex'>
+        <div className='md:h-[400px] w-full py-10 sm:py-2 md:px-10 px-8 md:flex'>
             <div className='h-[200px] w-full mb-5 md:w-1/2 md:h-full'>
 
                 <Map />
             </div>
             <div className=' w-full md:w-1/2 text-center'>
-                <h3 className='pb-10 text-xl md:text-6xl font-display'>Contact Us</h3>
+                <h3 className='pb-10 text-2xl md:text-6xl font-display'>Contact Us</h3>
 
                 <div className='flex hover:cursor-pointer items-center py-4 justify-center '>
                     <Link passHref={true} href='https://www.instagram.com/celestecakeshop/'>
