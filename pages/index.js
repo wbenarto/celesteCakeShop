11b3c3eaf964c9cffbfecf75e6c2bd3e9a8f8b77
w.benarto <w.benarto@gmail.com>
2022-08-26 15:10:46 -0700

    v2 done

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,12 @@ const Home = ({bannerData, products}) => {
         </h1>
       </div>
    
-        <div className='flex-row my-2 py-5 md:my-20 h-[20rem] px-4 gap-4 md:gap-14 md:px-20 lg:px-32 md:h-[600px]  '>
+        <div className='flex-row my-2 py-5 md:my-20 h-[24rem] px-4 gap-4 md:gap-14 md:px-20 lg:px-32 md:h-[30rem]  '>
           <div className='w-1/2 absolute  z-1 px-4 lg:px-0'>
             <img src='/images/bday1.jpg' alt='' className='border-4 border-white mt-[-80px] md:mt-[-120px] lg:max-h-[900px] md:p-2 lg:p-4 object-cover' ></img>
           </div>
-          <div className='w-3/4  relative mt-24 float-right bg-white p-4  '>
-            <h2 className='text-[#324d67] text-lg font-hero md:text-6xl'>Custom Cake Order</h2>
+          <div className='w-3/4 md:w-1/2  relative mt-32 float-right bg-white p-4  '>
+            <h2 className='text-[#324d67] text-lg font-hero md:text-4xl'>Custom Cake Order</h2>
             <p className='py-4 md:py-10 md:text-lg text-xs '>We take Custom Design Cake pre-orders for your special occassion. Send us your design! </p>
             <p className='font-bold text-sm'>Made especially for you</p>
             <Link href='/menu/custom'>
@@ -49,9 +49,9 @@ const Home = ({bannerData, products}) => {
         </div> */}
       
       
-      <div className='md:my-20 h-[24rem] md:h-[36rem] text-center mx-auto md:max-w-[1200px]'>
+      <div className='grid my-auto md:my-10 min-h-full h-[30rem] md:h-[36rem] text-center mx-auto md:max-w-[1200px]'>
         <div className='w-full h-[20rem] bg-white absolute mt-[10%] skew-y-[190deg]'></div>
-        <h2 className='relative text-[#324d67] pt-14 md:pt-8 md:pb-4 text-lg  font-hero md:text-4xl'>Other Baked Goods</h2>
+        <h2 className='relative text-[#324d67] pt-14 md:pt-8  text-lg  font-hero md:text-2xl lg:text-3xl'>Other Baked Goods</h2>
         <div className=' relative px-4 grid grid-cols-3 gap-[10px] md:gap-[4rem] mt-4 w-full '>
           {products.map((e,i) => (
             <div className=''>
