32d65db989a02824291ccbff7546fc9c559511f1
w.benarto <w.benarto@gmail.com>
2022-08-26 16:07:43 -0700

    utilize tailwind keyframes animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow':'spin 3s linear infinite',
+        'wiggle': 'wiggle 3s linear infinite'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    },
     fontFamily: {
       'sans': ['ui-sans-serif', 'system-ui'],
       'serif': ['ui-serif', 'Georgia'],
