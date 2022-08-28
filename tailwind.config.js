214feaba64ba2ccdd0d6cc50a562a38aeafc2548
w.benarto <w.benarto@gmail.com>
2022-08-28 09:15:56 -0700

    bg fit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
+    backgroundSize: { 
+      'auto': 'auto',
+      'contain': 'contain',
+      'cover': 'cover',
+      '50%': '50%',
+    },
     extend: {
       animation: {
         'spin-slow':'spin 3s linear infinite',
