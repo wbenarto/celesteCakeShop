7090d62101cb7c4010f0d3530591a06da198f5c0
w.benarto <w.benarto@gmail.com>
2022-11-15 13:51:40 -0800

    sanity studio deployed

---
+++ sanity_ecommerce/schemas/order.js
@@ -0,0 +1,49 @@
+export default {
+    name: 'orders',
+    title: 'Orders',
+    type: 'document',
+    fields: [
+      {
+        name: 'customer',
+        title: 'Customer Name',
+        type: 'string',
+      },
+      {
+        name: 'email',
+        title: 'Email',
+        type: 'string',
+      },
+      {
+        name: 'phone',
+        title: 'Phone',
+        type: 'number',
+      },
+      {
+        name: 'pickupDate',
+        title: 'Pickup Date',
+        type: 'text',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+      },
+      {
+        name: 'imageUrl',
+        title: 'Image URL',
+        type: 'text',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'array',
+        of: [
+          {
+            type: 'image',
+          },
+        ],
+        options: {hotspot: true}
+      },
+    ],
+  }
+  
