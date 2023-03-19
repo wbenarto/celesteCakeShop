abbb87f701dd3692b48aa70fc29f621747360a9b
w.benarto <w.benarto@gmail.com>
2023-03-19 10:45:35 -0700

    responsive fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,13 +18,7 @@ module.exports = {
     },
     screens: {
       sm: "450px",
-      md: [
-        // Sidebar appears at 768px, so revert to `sm:` styles between 768px
-        // and 868px, after which the main content area is wide enough again to
-        // apply the `md:` styles.
-        { min: "668px", max: "767px" },
-        { min: "868px" },
-      ],
+      md: "868px",
       lg: "1200px",
       xl: "1600px",
     },
