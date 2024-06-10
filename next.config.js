b3d526db54e4348c4aa45e87f22e4b2f2d8d6ac5
William Benarto <w.benarto@gmail.com>
2024-06-09 17:21:21 -0700

    swc

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
   reactStrictMode: true,
   optimizeFonts: false,
   swcMinify: false,
