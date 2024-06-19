446b59b95a6cabab2ef8bccdb07b4b63cdae1f62
William Benarto <w.benarto@gmail.com>
2024-06-19 11:45:41 -0700

    deploy

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: false,
   swcMinify: false,
-  eslint: {
-    dirs: ["src"],
-  },
   webpack(config) {
     config.infrastructureLogging = {debug: /PackFileCache/}
     return config;
